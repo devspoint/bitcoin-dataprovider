f41e177421388bd7771b67c207b153a02b12beca
Felipe Formentin <felipe.formentin@mercadolivre.com>
2021-11-03 18:17:18 -0300

    Initial Structure

---
+++ src/main/kotlin/br/com/devspoint/bitcoindataprovider/BitcoinDataproviderApplication.kt
@@ -0,0 +1,11 @@
+package br.com.devspoint.bitcoindataprovider
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BitcoinDataproviderApplication
+
+fun main(args: Array<String>) {
+	runApplication<BitcoinDataproviderApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/devspoint/bitcoindataprovider/BitcoinDataproviderApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.devspoint.bitcoindataprovider
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BitcoinDataproviderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
